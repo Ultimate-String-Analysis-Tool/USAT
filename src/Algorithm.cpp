fae9a248f51e4cd28033b00a601536765f3c62be
Micah <micah112358@gmail.com>
2020-05-31 15:39:54 -0500

    Implement Configuration class from libconfig in Algorithm

--- src/Algorithm.cpp
+++ src/Algorithm.cpp
@@ -7,3 +7,26 @@ void Algorithm::execute(){
     cout << "This algorithm has been executed" << endl;
     
 }
+
+Algorithm::Algorithm() {
+    populateSpecsFromFile();
+}
+
+int Algorithm::populateSpecsFromFile() {
+    // Read the file. If there is an error, report it and exit.
+    try
+    {
+        config.readFile("example.cfg");
+    }
+    catch(const FileIOException &fioex)
+    {
+        cerr << "I/O error while reading file." << endl;
+        return(EXIT_FAILURE);
+    }
+    catch(const ParseException &pex)
+    {
+        cerr << "Parse error at " << pex.getFile() << ":" << pex.getLine()
+                    << " - " << pex.getError() << endl;
+        return(EXIT_FAILURE);
+    }
+}
--- src/Algorithm.hpp
+++ src/Algorithm.hpp
@@ -1,8 +1,22 @@
 #pragma once
 
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+#include <libconfig.h++>
+
+
+using namespace std;
+using namespace libconfig;
+
+
 class Algorithm{
+    Config config;
+
     public:
+        Algorithm();
         void execute();
     private:
+        int populateSpecsFromFile();
 
 };
